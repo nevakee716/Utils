7e567810a205d5aa90913f5de8e8869d35488c8b
nevakee716 <nevakee716@gmail.com>
2019-10-23 16:09:59 +0200

    version 1.6

--- src/utils.js
+++ src/utils.js
@@ -508,7 +508,7 @@
   if (cwAPI.customLibs.utils === undefined) {
     cwAPI.customLibs.utils = {};
   }
-  cwAPI.customLibs.utils.version = 1.5;
+  cwAPI.customLibs.utils.version = 1.6;
   cwAPI.customLibs.utils.layoutsByNodeId = {};
   cwAPI.customLibs.utils.getItemDisplayString = getItemDisplayString;
   cwAPI.customLibs.utils.manageHiddenNodes = manageHiddenNodes;
